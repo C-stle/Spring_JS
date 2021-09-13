cbb05241b6d34f6315af41094738f2605cdeeb36
Choi Seong Hoon <sunghun5620@gmail.com>
2021-09-13 16:31:30 +0900

    [변경사항] 1. 메인 화면 추가

--- src/main/java/kr/co/rmsoft/rfid/web/controller/view/rfid/sts/StsViewRouter.java
+++ src/main/java/kr/co/rmsoft/rfid/web/controller/view/rfid/sts/StsViewRouter.java
@@ -1,12 +1,19 @@
-package kr.co.rmsoft.web.controller.view.rfid.sts;
+package kr.co.rmsoft.rfid.web.controller.view.rfid.sts;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Slf4j
 @Controller
 public class StsViewRouter {
 
+    /**
+     * 메인 화면
+     * @param model
+     * @return
+     */
     @GetMapping("/rfid/sts/main")
     public String mainView(ModelMap model) {
         return "views/rfid/sts/main";
